d8d7184ad1f128108fdc92fc53df3743a28d3122
Akshat Gupta <akshatgupta001@live.com>
2019-08-29 22:42:14 +0530

    Initial Commit

---
+++ Music Player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Music Player
+//
+//  Created by Akshat Gupta on 29/08/19.
+//  Copyright © 2019 coded. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
